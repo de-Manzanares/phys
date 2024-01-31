dca58c50a2e1b67328ca9235ae316936afee5684
de-Manzanares <138251011+de-Manzanares@users.noreply.github.com>
2024-01-30 18:56:33 -0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-#include "FluidContainer.h"
+#include "StandingWavesOnAString.h"
 
 int main()
 {
+    StandingWavesOnAString sim;
+
+    sim.list_node_properties_massless(15);
+    sim.list_node_properties_fixed_linear_density(15);
 
     return 0;
 }
