7a5652caf8a9a725bee819daa8dfeeb52d0c6755
de-Manzanares <138251011+de-Manzanares@users.noreply.github.com>
2024-01-30 17:30:24 -0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,8 @@
-void
-shmBuoyancy(double particleDensity, double particleVolume, double fluidDensity,
-        double containerDepth, double particleStartPosition);
-
 #include <iostream>
+#include "FluidContainer.h"
 
 int main()
 {
-    shmBuoyancy(800, 1, 1000, 1, -0.5);
-    std::cout << "\n";
-    shmBuoyancy(2712, 1, 13500, 1, -0.5);
-    std::cout << "\n";
-    shmBuoyancy(800, 1, 1000, 1, 1);
+
     return 0;
 }
